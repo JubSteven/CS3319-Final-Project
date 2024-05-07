5c256cc0d1ee50d231c714e9dd28ccb121ede4d0
JubSteven <1120395085@qq.com>
2024-05-07 19:01:42 +0800

    Fix bug with edges

--- run.py
+++ run.py
@@ -6,17 +6,24 @@ from baseline import inference_wrapper
 from dataset import *
 
 raw_data = torch.load("data\data.pt")
-graph_files = os.listdir("graphs")
 data_loader = GraphData("data\data.pt")
 
-for file in graph_files:
-    if "logits" not in file:
-        continue
+graph_aug = "graph_3_logits.pkl"  # You can change this to a different graph
 
-    with open(os.path.join("graphs", file), "rb") as f:
-        adj = pickle.load(f)
+with open(os.path.join("graphs", graph_aug), "rb") as f:
+    adj = pickle.load(f)
 
-    edges = sample_graph_det(data_loader.adj_train.to_dense().numpy(), adj)
+remove_edges = [100, 200, 300, 400, 500, 600]
+edge_list = []
+for enum in remove_edges:
+    edges = sample_graph_det(data_loader.adj_train.to_dense().numpy(), adj, enum)
     raw_data.edge_index = torch.from_numpy(edges.T)
     result = inference_wrapper(raw_data, "model.pt")
-    print(result)
+    print(f"Result when {enum} edges removed: {result}, current edge count: {raw_data.edge_index.shape[1] // 2}")
+
+    edge_list.append(edges.T.reshape(-1).tolist())
+
+df = pd.DataFrame(edge_list).fillna(-1).astype(int)
+# fill those empty units with -1 (don't change it)
+df.insert(0, 'ID', list(range(len(edge_list))))
+df.to_csv('submission.csv', index=False)
--- utils.py
+++ utils.py
@@ -99,14 +99,13 @@ def train_model(cfg, graph_data, model):
 
 
 # remove_pct and add_pct are two hyperparameters that control the number of edges to be removed and added
-def sample_graph_det(adj_orig, A_pred, remove_percentage=5, add_percentage=5):
-    if remove_percentage == 0 and add_percentage == 0:
+def sample_graph_det(adj_orig, A_pred, remove_edge_num=100):
+    if remove_edge_num == 0:
         return copy.deepcopy(adj_orig)
     orig_upper = sp.triu(adj_orig, 1)
-    n_edges = orig_upper.nnz
     edges = np.asarray(orig_upper.nonzero()).T
-    if remove_percentage:
-        n_remove = int(n_edges * remove_percentage / 100)
+    if remove_edge_num:
+        n_remove = remove_edge_num
         pos_probs = A_pred[edges.T[0], edges.T[1]]
         e_index_2b_remove = np.argpartition(pos_probs, n_remove)[:n_remove]
         mask = np.ones(len(edges), dtype=bool)
@@ -115,22 +114,9 @@ def sample_graph_det(adj_orig, A_pred, remove_percentage=5, add_percentage=5):
     else:
         edges_pred = edges
 
-    if add_percentage:
-        n_add = int(n_edges * add_percentage / 100)
-        # deep copy to avoid modifying A_pred
-        A_probs = np.array(A_pred)
-        # make the probabilities of the lower half to be zero (including diagonal)
-        A_probs[np.tril_indices(A_probs.shape[0])] = 0
-        # make the probabilities of existing edges to be zero
-        A_probs[edges.T[0], edges.T[1]] = 0
-        all_probs = A_probs.reshape(-1)
-        e_index_2b_add = np.argpartition(all_probs, -n_add)[-n_add:]
-        new_edges = []
-        for index in e_index_2b_add:
-            i = int(index / A_probs.shape[0])
-            j = index % A_probs.shape[0]
-            new_edges.append([i, j])
-        edges_pred = np.concatenate((edges_pred, new_edges), axis=0)
+    # Recover the edges to [a,b] and [b,a] instead of [a,b]
+    edges_pred = np.concatenate([edges_pred, edges_pred[:, ::-1]])
+
     return edges_pred
 
 

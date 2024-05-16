c4e569edff4bd06a65fced2455e6ee6f208a39c5
JubSteven <1120395085@qq.com>
2024-05-16 15:12:58 +0800

    modify run.py

--- run.py
+++ run.py
@@ -5,39 +5,72 @@ from utils import sample_graph_det
 from baseline import inference_wrapper
 from dataset import *
 
-raw_data = torch.load("data\data.pt")
-data_loader = GraphData("data\data.pt")
-model_path = os.listdir("models")
 
-graph_aug = "graph_1_logits.pkl"  # NOTE: You can change this to a different graph
+def from_graph():
+    raw_data = torch.load("data\data.pt")
+    data_loader = GraphData("data\data.pt")
+    model_path = os.listdir("models")
 
-with open(os.path.join("graphs", graph_aug), "rb") as f:
-    adj = pickle.load(f)
+    graph_aug = "graph_4_logits.pkl"  # NOTE: You can change this to a different graph
 
-# No AUG - baseline (no delete edges)
-# [0.792, 0.796, 0.808, 0.796, 0.794, 0.800]
+    with open(os.path.join("graphs", graph_aug), "rb") as f:
+        adj = pickle.load(f)
 
-remove_edges = [100, 200, 300, 400, 500, 600]
-edge_list = []
-means = []
-for enum in remove_edges:
-    edges = sample_graph_det(data_loader.adj_train, adj, enum)
-    raw_data.edge_index = torch.from_numpy(edges.T)
+    # No AUG - baseline (no delete edges)
+    # [0.792, 0.796, 0.808, 0.796, 0.794, 0.800]
 
-    val_acc = []
-    for model in model_path:
-        result = inference_wrapper(raw_data, os.path.join("models", model))
-        val_acc.append(round(result["val_acc"].item(), ndigits=3))
-    mean = np.array(val_acc).mean().round(3)
-    print(f"Result when {enum} edges removed: Mean {mean} | Original: {val_acc}")
+    remove_edges = [100, 200, 300, 400, 500, 600]
+    edge_list = []
+    means = []
+    for enum in remove_edges:
+        edges = sample_graph_det(data_loader.adj_train, adj, enum)
+        raw_data.edge_index = torch.from_numpy(edges.T)
 
-    means.append(mean)
-    edge_list.append(edges.T.reshape(-1).tolist())
+        val_acc = []
+        for model in model_path:
+            result = inference_wrapper(raw_data, os.path.join("models", model))
+            val_acc.append(round(result["val_acc"].item(), ndigits=3))
+        mean = np.array(val_acc).mean().round(3)
+        print(f"Result when {enum} edges removed: Mean {mean} | Original: {val_acc}")
 
-print("Overall mean:", np.array(means).mean().round(4))
+        means.append(mean)
+        edge_list.append(edges.T.reshape(-1).tolist())
 
-# NOTE: Don't change this, used for generating the submission csv
-df = pd.DataFrame(edge_list).fillna(-1).astype(int)
-# fill those empty units with -1 (don't change it)
-df.insert(0, 'ID', list(range(len(edge_list))))
-df.to_csv('submission.csv', index=False)
+    print("Overall mean:", np.array(means).mean().round(4))
+
+    # NOTE: Don't change this, used for generating the submission csv
+    df = pd.DataFrame(edge_list).fillna(-1).astype(int)
+    # fill those empty units with -1 (don't change it)
+    df.insert(0, 'ID', list(range(len(edge_list))))
+    df.to_csv('submission.csv', index=False)
+
+
+def from_submission():
+    raw_data = torch.load("data\data.pt")
+    model_path = os.listdir("models")
+
+    df = pd.read_csv('submission.csv')
+    edge_arr = df.to_numpy()[:, 1:]
+    edge_list = []
+    for i in range(edge_arr.shape[0]):
+        edge_list.append(edge_arr[i][edge_arr[i] != -1].reshape(2, -1))
+
+    means = []
+
+    for edge in edge_list:
+        raw_data.edge_index = torch.from_numpy(edge)
+
+        val_acc = []
+        for model in model_path:
+            result = inference_wrapper(raw_data, os.path.join("models", model))
+            val_acc.append(round(result["val_acc"].item(), ndigits=3))
+        mean = np.array(val_acc).mean().round(3)
+        means.append(mean)
+        print(f"Result when of {edge.shape[1] // 2} edges: Mean {mean} | Original: {val_acc}")
+
+    print("Overall mean:", np.array(means).mean().round(4))
+
+
+if __name__ == "__main__":
+    # from_graph()
+    from_submission()

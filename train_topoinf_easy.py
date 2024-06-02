c9a5ce10a3c1a9c2ab24b18214dc4f6a25607b34
yizhou0409 <yizhou0409@sjtu.edu.cn>
2024-06-02 21:14:48 +0800

    topoinf easy

--- train_topoinf_easy.py
+++ train_topoinf_easy.py
@@ -1,12 +1,11 @@
 from baseline import train_wrapper, GCN_Net
 import torch
 from torch_geometric.utils import to_networkx, from_networkx
-from itertools import combinations
 import networkx as nx
 from tqdm import tqdm
-import math
 import numpy as np
 import heapq
+device = "mps"
 
 def get_all_edges(A):
     """
@@ -28,19 +27,31 @@ def get_all_edges(A):
 
     return edges
 
+def f_GPU(A, L):
+    # Convert input matrix to PyTorch Tensor and move it to the GPU
+    A = A.astype(np.float32) # mps only supports float32
+    A_tensor = torch.from_numpy(A).to(device=device)
+    I_tensor = torch.eye(A_tensor.shape[0], device=device)
+    # Perform the matrix operations on the GPU
+    A_squared_tensor = torch.matmul(A_tensor, A_tensor)
+    result_tensor = A_squared_tensor + 2 * A_tensor + I_tensor
+    result_tensor = torch.matmul(result_tensor, L)
+    # Convert the result back to a NumPy ndarray
+    return result_tensor
+
 def I(A, L):
-    E = np.eye(A.shape[0])
-    fAl = np.dot(np.linalg.matrix_power(A+E, 2), L)
-    norms_L = np.linalg.norm(L)
-    norms_f = np.linalg.norm(fAl)
-    IA = np.dot(L, fAl) / (norms_L * norms_f)
+    L = L.float()
+    fAl = f_GPU(A, L)
+    norms_f = torch.norm(fAl)
+    norms_L = torch.norm(L)
+    IA = torch.matmul(L, fAl.t()) / (norms_L * norms_f)
     return IA
 
 def topoinf(A, u, v, labels): #topoinf for edge e_{uv}
     A_ = A.copy()
     A_[u][v] = 0
     A_[v][u] = 0
-    return I(A, labels)
+    return I(A_, labels)
 
 def top_n_edges(A, n, labels):
     """
@@ -78,11 +89,11 @@ def adjust_graph_topology_topoinf_easy(data, model_path='model.pt', edge_to_remo
     """
     model = GCN_Net(2, data.num_features, 32, 7, 0.4)  # NOTE: cannot change
     model.load_state_dict(torch.load(model_path))
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    device = torch.device("mps")
     model.to(device)
     data.to(device)
     # Adjust the graph topology
-    labels = data.Y
+    labels = data.y
     G = to_networkx(data, to_undirected=True)
     adj_t = nx.to_numpy_array(G)
     edges_to_remove = top_n_edges(adj_t, edge_to_remove, labels)

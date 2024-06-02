597aa54191654b45e187127a459ba50d0b5435f0
huskydoge <hbh001098hbh@sjtu.edu.cn>
2024-06-02 15:03:02 +0800

    lamda tuning

--- run.py
+++ run.py
@@ -2,7 +2,7 @@
 Author: huskydoge hbh001098hbh@sjtu.edu.cn
 Date: 2024-05-31 15:33:13
 LastEditors: huskydoge hbh001098hbh@sjtu.edu.cn
-LastEditTime: 2024-06-02 13:32:31
+LastEditTime: 2024-06-02 14:38:07
 FilePath: /GNN/CS3319-Final-Project/run.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -176,5 +176,5 @@ def from_topoinf(lamda = 0.1):
 if __name__ == "__main__":
     # from_graph()
     # from_submission()
-    # from_adaedge()
-    from_topoinf(0.5)
+    from_adaedge()
+    # from_topoinf(0.3)
